b8f426cd92d665e0bf639dddb409aed93fd1dd75
denopendabot <denopendabot@github.com>
2023-05-09 22:04:05 +0000

    build(deps): bump std from 0.185.0 to 0.186.0

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.185.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.186.0/testing/asserts.ts";
 import { main } from "./mod.ts";
 
 Deno.test("hello()", () => {
